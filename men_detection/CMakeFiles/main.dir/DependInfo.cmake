fc5e533ace4ebedde571fefe496b731081fc97ac
Touru97 <47893261+Touru97@users.noreply.github.com>
2021-04-19 00:03:34 +0100

    Add files via upload

---
+++ men_detection/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/men_detection/body_detect.cpp" "/home/pi/men_detection/CMakeFiles/main.dir/body_detect.cpp.o"
+  "/home/pi/men_detection/main.cpp" "/home/pi/men_detection/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ men_detection/body_detect.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <wiringPi.h>
+using namespace std;
+
+class body_detect
+{
+	public:
+		void human_detection();
+};
